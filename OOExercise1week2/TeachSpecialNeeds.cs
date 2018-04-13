9412eb93168f22a910d0c8a07c52bc35e7e83e39
Adrian Phillips <33710156+AP101644658@users.noreply.github.com>
2018-04-13 10:30:12 +1000

    Add files via upload

---
+++ OOExercise1week2/TeachSpecialNeeds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOExercise1week2
+{
+    class TeachSpecialNeeds:ITeach
+    {
+        public string Teach()
+        {
+            return " teach special needs";
+        }
+    }
+}
